ded71ef4e01a70531588377f6f112eeae0c22a71
Vinitha <singamvineetha24@gmail.com>
2026-07-31 21:32:45 +0530

    Add course display function with annotations

--- Week1/3_annotations-demo.ts
+++ Week1/3_annotations-demo.ts
@@ -1,1 +1,9 @@
+let course: string = "Full Stack Development";
+let duration: number = 6;
 
+function displayCourse(name: string, months: number): void {
+    console.log("Course:", name);
+    console.log("Duration:", months, "months");
+}
+
+displayCourse(course, duration);
