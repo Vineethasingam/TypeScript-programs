062344436a15a133ce538745f582200b65e5548b
Vinitha <singamvineetha24@gmail.com>
2026-07-31 23:08:29 +0530

    Implement NumberOperations class with GCD and LCM

--- week3/3_readonly-static.ts
+++ week3/3_readonly-static.ts
@@ -1,1 +1,37 @@
+// Concept: readonly and static using GCD and LCM
 
+class NumberOperations {
+    readonly num1: number;
+    readonly num2: number;
+
+    static collegeName: string = "SVECW";
+
+    constructor(a: number, b: number) {
+        this.num1 = a;
+        this.num2 = b;
+    }
+
+    findGCD(): number {
+        let a = this.num1;
+        let b = this.num2;
+
+        while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    findLCM(): number {
+        return (this.num1 * this.num2) / this.findGCD();
+    }
+}
+
+const obj = new NumberOperations(20, 30);
+
+console.log(`College: ${NumberOperations.collegeName}`);
+console.log(`Numbers: ${obj.num1}, ${obj.num2}`);
+console.log(`GCD = ${obj.findGCD()}`);
+console.log(`LCM = ${obj.findLCM()}`);
